51074e1d9bc1c568913314df67439fa54cb000bb
kgSuzuki1994 <takuti0511@gmail.com>
2020-01-04 21:46:10 +0900

    [add] listviewとmodelのデータを連携

--- boardapp/views.py
+++ boardapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
+from .models import BoardModel
 
 # Create your views here.
 def signupfunc(request):
@@ -29,4 +30,5 @@ def loginfunc(request):
     return render(request, 'login.html')
 
 def listfunc(request):
-    return render(request, 'list.html')
+    object_list = BoardModel.objects.all()
+    return render(request, 'list.html', {'object_list': object_list})
